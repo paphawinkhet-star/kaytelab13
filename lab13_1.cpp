7f30e6fbbcdc33a09a184f5aa9a98c757e51e4ce
paphawinkhet-star <paphawin.khet@gmail.com>
2026-01-09 20:08:28 +0700

    complete README.md lab13_1.cpp lab13_2.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,36 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[], int N, double B[]){
+    double sum = 0.0;
+    double product = 1.0;
+    double reciprocalSum = 0.0;
+
+    double maxVal = A[0];
+    double minVal = A[0];
+
+    for(int i = 0; i < N; i++){
+        sum += A[i];
+        product *= A[i];
+        reciprocalSum += 1.0 / A[i];
+
+        if(A[i] > maxVal)
+            maxVal = A[i];
+        if(A[i] < minVal)
+            minVal = A[i];
+    }
+
+    double mean = sum / N;
+
+    double varianceSum = 0.0;
+    for(int i = 0; i < N; i++){
+        varianceSum += pow(A[i] - mean, 2);
+    }
+
+    B[0] = mean;
+    B[1] = sqrt(varianceSum / N);
+    B[2] = pow(product, 1.0 / N);
+    B[3] = N / reciprocalSum;
+    B[4] = maxVal;
+    B[5] = minVal;
+}
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,34 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool image[][M], int s, int x, int y){
+    if(s == 0 && x == 0 && y == 0)
+        return;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            double dist = sqrt((i - x)*(i - x) + (j - y)*(j - y));
+            if(dist <= s - 1){
+                image[i][j] = true;
+            }
+        }
+    }
+}
+void showImage(const bool image[][M]){
+    for(int i = 0; i < M + 2; i++)
+        cout << "-";
+    cout << endl;
+    for(int i = 0; i < N; i++){
+        cout << "|";
+        for(int j = 0; j < M; j++){
+            if(image[i][j])
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "|" << endl;
+    }
+    for(int i = 0; i < M + 2; i++)
+        cout << "-";
+    cout << endl;
+}
